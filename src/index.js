145d3a6778d65546a0a1e40d74d82d070df4682b
Gideon <gideon.ubaldo@sjsu.edu>
2018-09-20 14:48:39 -0700

    hot module and backbone fololders

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App/>, rootEl)
+}
+
+if(module.hot){
+     module.hot.accept('./App', () => {
+         setTimeout(render)
+     })
+}
+
+render();
+
 registerServiceWorker();
