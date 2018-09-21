b8f170a51adb20192c28b60dbd9d52f6ca68d34e
Gideon <gideon.ubaldo@sjsu.edu>
2018-09-21 11:13:47 -0700

    deleted common folders, corrected register form, added package for routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import NavBar from '../src/features/nav/navbar/NavBar';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import EventDashBoard from "./features/event/eventdashboard/EventDashboard";
 
 class App extends Component {
   render() {
     return (
-      <NavBar/>
-    )
+      <div>
+        <EventDashBoard />
+      </div>
+    );
   }
 }
 
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Theme from '../../../theme';
-import { MuiThemeProvider } from '@material-ui/core';
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Theme from "../../../theme";
+import { MuiThemeProvider } from "@material-ui/core";
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    const { classes } = this.props;
     return (
       <MuiThemeProvider theme={Theme}>
-        <AppBar position="static">
-            <Toolbar style={Theme.palette.tertiary_orange}>
-              <Typography variant="headline" color="inherit">
+        <AppBar position="static" color="primary">
+          <Toolbar style={Theme.palette.tertiary_orange}>
+            <Typography variant="headline" color="inherit">
               StayInn
-              </Typography>
-            </Toolbar>
-          </AppBar>
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </MuiThemeProvider>
-    ); 
+    );
   }
 }
 
