7729b4bd9ade22dd564029e3a4ff2832aaae746b
Gideon <gideon.ubaldo@sjsu.edu>
2018-09-22 10:48:51 -0700

    naming convention: First letter for folder would be small letter and files first letter would be capitalized

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import EventDashBoard from "./features/event/eventdashboard/EventDashboard";
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import theme from './theme';
-import NavBar from './features/nav/Navbar/NavBar'
+import NavBar from './features/nav/navbar/NavBar'
 
 class App extends Component {
   render() {
