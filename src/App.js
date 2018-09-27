82e19fad4ffa9cc436e827122f55dc6ab256d630
Philip Nguyen <vietwhammies@gmail.com>
2018-09-27 11:45:40 -0700

    naming conventions are now followed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 // import "./App.css";
 import EventDashBoard from "./features/event/eventdashboard/EventDashboard";
+import { MuiThemeProvider, createMuiTheme }from '@material-ui/core/styles';
+import NavBar from "./features/nav/navbar/NavBar";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#409be6'
+    },
+  secondary: {
+      main: '#35495d'
+    },
+  }
+})
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-      <NavBar theme={theme}/>
+      <NavBar color="primary"/>
       </MuiThemeProvider>
     )
   }
