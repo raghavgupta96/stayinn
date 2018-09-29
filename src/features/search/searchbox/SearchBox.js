ebbe7d995e063ae7077c25dabadf304d1cea2ec5
yunjun ma <jun@yunjuns-MacBook-Pro.local>
2018-09-28 22:48:08 -0700

    Added an autocomplete search bar, including a state update method

--- src/features/search/searchbox/SearchBox.js
+++ src/features/search/searchbox/SearchBox.js
@@ -2,6 +2,10 @@ import Calendar from "../../../app/calendar/Calendar";
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import Autocomplete from 'react-google-autocomplete';
+
+// const API_KEY = GOOGLE_API_KEY;
+// const API_KEY = "AIzaSyBAaov165t1KK89TiqHPLjM1poFNHJk8OY"
 
 class SearchBox extends Component {
     state = {
@@ -13,7 +17,16 @@ class SearchBox extends Component {
   };
   render() {
     return (
+
       <div>
+              <Autocomplete
+      style={{width: '90%'}}
+      onPlaceSelected={(place) => {
+        console.log(place);
+      }}
+      types={['(regions)']}
+      componentRestrictions={{country: "ru"}}
+      />
         <TextField
           id="standard-search"
           label="Where are you going?"
