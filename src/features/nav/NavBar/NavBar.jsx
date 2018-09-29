0d23f8a04d45293520b30c44a4c370056602215c
Alan Boo <oobnala@Alans-MacBook-Pro.local>
2018-09-28 21:37:10 -0700

    added signout function in NavBar

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { withFirebase } from 'react-redux-firebase';
 import { NavLink, withRouter } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
+const mapState = (state) => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile
+})
+
 // style={{ flex: 1 }} pushes the subsequent things to the right
 const NavBar = theme => {
+
+  const handleSignOut = () => {
+    this.props.firebase.logout();
+    //this.props.history.push('/');
+  }
   return (
     <AppBar position="static">
       <Toolbar>
@@ -22,4 +34,4 @@ const NavBar = theme => {
   )
 }
 
-export default withRouter(NavBar);
+export default withRouter(withFirebase(connect(mapState)(NavBar)));
