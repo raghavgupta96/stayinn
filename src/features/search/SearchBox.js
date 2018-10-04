8396d06eb031b3e63c4ffccf40f5f5e54b48a99f
Gideon <gideon.ubaldo@sjsu.edu>
2018-10-04 10:40:11 -0700

    search box css update

--- src/features/search/SearchBox.js
+++ src/features/search/SearchBox.js
@@ -7,6 +7,9 @@ import Autocomplete from "react-google-autocomplete";
 import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
 
 const styles = theme => ({
   root: {
@@ -14,16 +17,17 @@ const styles = theme => ({
   },
   paper: {
     width: "100%",
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingTop: 20,
-    paddingBottom: 20,
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingBottom: 15,
+    paddingTop: 10,
     marginTop: 100,
     margin: 20,
     color: theme.palette.text.secondary
   },
-  item: {
-    paddingLeft: 100
+  formControl: {
+    minWidth: 50,
+    paddingTop: 5
   }
 });
 
@@ -33,6 +37,7 @@ class SearchBox extends Component {
     this.state = {
       checkinDate: null,
       checkoutDate: null,
+      roomnumber: "",
       badsNumber: [{ number: 1 }, { number: 2 }]
     };
   }
@@ -51,41 +56,65 @@ class SearchBox extends Component {
     console.log(this.state.checkoutDate);
   };
 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   submit = () => {
     console.log("submitted");
     //do functional here
     console.log("Checkin Date" + this.state.checkinDate);
     console.log("Checkout Date" + this.state.checkoutDate);
   };
+
   render() {
     const { classes } = this.props;
     return (
       <Grid container className={classes.root} spacing={16}>
         <Grid item sm={1} />
         <Grid item sm={9}>
-          <Paper
-            className={classes.paper}
-            style={{
-              //backgroundColor: "#409BE6"
-            }}
-          >
+          <Paper className={classes.paper}>
             <Grid container>
-              <Grid container style={{
-                paddingBottom: 5
-              }}>
-                <Typography
-                  variant="title"
-                  gutterBottom
-                  color="inherit"
+              <Grid sm={12}>
+                <Grid
+                  container
                   style={{
-                    //color: "#ffffff",
-                    fontFamily: "Times",
-                    fontWeight: "bold"
+                    paddingBottom: 5
                   }}
                 >
-                  Room Size:
-                </Typography>
-                <TextField id="standard-select-currency-native" select />
+                  <Grid sm={1}>
+                    <Typography
+                      variant="title"
+                      gutterBottom
+                      color="inherit"
+                      style={{
+                        fontFamily: "Times",
+                        fontSize: 20,
+                        lineHeight: 1,
+                        paddingTop: 10
+                      }}
+                    >
+                      Room Size:
+                    </Typography>
+                  </Grid>
+                  <Grid sm={2}>
+                    <FormControl className={classes.formControl}>
+                      <Select
+                        value={this.state.roomnumber}
+                        onChange={this.handleChange}
+                        displayEmpty
+                        name="roomnumber"
+                        className={classes.selectEmpty}
+                      >
+                        <MenuItem value="">1</MenuItem>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                        <MenuItem value={4}>4</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                </Grid>
               </Grid>
               <Grid
                 item
@@ -99,7 +128,6 @@ class SearchBox extends Component {
                     width: "100%",
                     innerHeight: "100",
                     height: 45,
-                    //backgroundColor: "#E6F6FF",
                     fontSize: 18,
                     fontFamily: "Times"
                   }}
@@ -116,7 +144,6 @@ class SearchBox extends Component {
               <Grid item sm={2}>
                 <Paper
                   style={{
-                    //backgroundColor: "#E6F6FF",
                     marginRight: 20
                   }}
                 >
@@ -135,7 +162,6 @@ class SearchBox extends Component {
               <Grid item sm={2}>
                 <Paper
                   style={{
-                    //backgroundColor: "#E6F6FF",
                     marginRight: 20
                   }}
                 >
@@ -156,7 +182,6 @@ class SearchBox extends Component {
                   size="medium"
                   style={{
                     backgroundColor: "#ffffff",
-                    //color: "#409BE6",
                     height: 47
                   }}
                   variant="contained"
