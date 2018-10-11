614b10732b3c551b9e3332e70169ba54b230c4cd
Chad Gorham <gorhamchadsbhs@gmail.com>
2018-10-11 15:25:56 -0700

    Changed the set to update in DB--- VERY IMPORTANT UPDATE

--- src/features/payment/PaymentBackend.js
+++ src/features/payment/PaymentBackend.js
@@ -1,13 +1,30 @@
+import {registerUser} from '../auth/authActions'
+
+//var stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+
+//const token = request.body.stripeToken; // Using Express
+
+//const charge = stripe.charges.create({
+//  amount: 999,
+//  currency: 'usd',
+//  description: 'Example charge',
+//  source: token,
+//});
+
 export const inputCard = (card) => {
-  return(dispatch, getState, {getFirebase, getFirestore}) => {
+  return async (dispatch, getState, {getFirebase, getFirestore}) => {
     const firestore = getFirestore()
-    firestore.set(`users/card`,{
-      ...card,
-      cardname: card.cardname,
+    //const firebase = getFirebase()
+
+    //await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password);
+    //firebase.firestore().collection("card",{
+    firestore.update(`users/Q9mWtevzhpPXWiXcO1xCZGU80ts1/card`, {card
+     /* cardname: card.cardname,
       cardnumber: card.cardnumber,
       cvc: card.cvc,
       expirymonth: card.expirymonth,
       expiryyear: card.expiryyear
+      */
     }).then(() =>{
       dispatch({ type:'INPUT_CARD', card});
     }).catch((error) => {
