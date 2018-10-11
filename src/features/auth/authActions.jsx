7a964edac2a6cbd44e5548d643ba5fbae57d2da4
Che Liu <richard14942003@gmail.com>
2018-10-10 17:15:31 -0700

    added update information form, able to update name, phone, and profile image. Put a redirect route when user is trying to type /profile to go to profile page without loggin in .

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -1,12 +1,15 @@
 import { SIGN_OUT_USER } from "./authConstants";
-import { SubmissionError } from 'redux-form'
+import { SubmissionError } from "redux-form";
 
 export const login = creds => {
   return async (dispatch, getState, { getFirebase }) => {
     const firebase = getFirebase();
 
     try {
-        await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password).then(function (creds) {
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(creds.email, creds.password)
+        .then(function(creds) {
           const user = firebase.auth().currentUser;
           const verified = user.emailVerified;
           console.log(user);
@@ -17,23 +20,22 @@ export const login = creds => {
             console.log("Email not verified homie!");
           }
         });
-        // const user = firebase.auth().currentUser;
-        // const verified = user.emailVerified;
-        // firebase.auth().signInWithEmailAndPassword(creds.email, creds.password);
-        //   console.log(user);
-        //   if (verified) {
-        //     console.log("You're in!");
-        //   } else {
-        //     console.log("Email not verified homie!");
-        //   }
-    
+      // const user = firebase.auth().currentUser;
+      // const verified = user.emailVerified;
+      // firebase.auth().signInWithEmailAndPassword(creds.email, creds.password);
+      //   console.log(user);
+      //   if (verified) {
+      //     console.log("You're in!");
+      //   } else {
+      //     console.log("Email not verified homie!");
+      //   }
     } catch (error) {
       console.log(error);
       throw new SubmissionError({
         _error: error.message
-      })
+      });
     }
-  }
+  };
 };
 
 export const logout = () => {
@@ -42,7 +44,7 @@ export const logout = () => {
   };
 };
 
-export const registerUser = (user) => async (
+export const registerUser = user => async (
   dispatch,
   getState,
   { getFirebase, getFirestore }
@@ -56,63 +58,26 @@ export const registerUser = (user) => async (
       .createUserWithEmailAndPassword(user.email, user.password);
     console.log(createdUser);
     // update the auth profile
-    await createdUser.sendEmailVerification().then(function() {
-      // Email sent.
-    }).catch(function(error) {
-      // An error happened.
-    });
+    await createdUser
+      .sendEmailVerification()
+      .then(function() {
+        // Email sent.
+      })
+      .catch(function(error) {
+        // An error happened.
+      });
     await createdUser.updateProfile({
-        displayName: user.displayName,
-    })
-
-    // var storageRef = firebase.storage().ref().child(createdUser.uid);
-    // // await storageRef.put(user.photoFile[0]).then(function(snapshot) {
-    // //   console.log('Uploaded a blob or file!');
-    // // });
-
-    // var uploadTask = storageRef.put(user.photoFile[0]);
-
-    // // Register three observers:
-    // // 1. 'state_changed' observer, called any time the state changes
-    // // 2. Error observer, called on failure
-    // // 3. Completion observer, called on successful completion
-    // uploadTask.on('state_changed', function(snapshot){
-    //   // Observe state change events such as progress, pause, and resume
-    //   // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    //   var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //   console.log('Upload is ' + progress + '% done');
-    //   switch (snapshot.state) 
-    //   {
-    //     case firebase.storage.TaskState.PAUSED: // or 'paused'
-    //       console.log('Upload is paused');
-    //       break;
-    //     case firebase.storage.TaskState.RUNNING: // or 'running'
-    //       console.log('Upload is running');
-    //       break;
-    //     default:
-    //       console.log('continue....')
-    //   }
-    // }, function(error) {
-    //   // Handle unsuccessful uploads
-    // }, function() {
-    //   // Handle successful uploads on complete
-    //   // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    //   uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-    //     console.log('File available at', downloadURL);
-    //     createdUser.updateProfile({
-    //       photoURL: downloadURL
-    //   })
-    //   });
-    // });
+      displayName: user.displayName
+    });
 
     // create a new profile in firestore
     let newUser = {
-        displayName: user.displayName,
-        createdAt: firestore.FieldValue.serverTimestamp(),
-        email: user.email
+      displayName: user.displayName,
+      createdAt: firestore.FieldValue.serverTimestamp(),
+      email: user.email
     };
 
-    await firestore.set(`users/${createdUser.uid}`, {...newUser});
+    await firestore.set(`users/${createdUser.uid}`, { ...newUser });
     window.location.href = "/";
     firebase.logout();
   } catch (error) {
@@ -123,7 +88,7 @@ export const registerUser = (user) => async (
   }
 };
 
-export const updateUser = (user) => async (
+export const updateUser = user => async (
   dispatch,
   getState,
   { getFirebase, getFirestore }
@@ -131,31 +96,51 @@ export const updateUser = (user) => async (
   const firebase = getFirebase();
   const firestore = getFirestore();
   try {
-    
     const currentUser = firebase.auth().currentUser;
 
-    if(user.displayName)
-    {
+    if (user.displayName) {
       await currentUser.updateProfile({
-        displayName: user.displayName,
-      })
-      firebase.firestore().collection('users').doc(currentUser.uid).update({
         displayName: user.displayName
       });
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(currentUser.uid)
+        .update({
+          displayName: user.displayName
+        })
+        .then(function() {
+          window.location.href = "/profile";
+        })
+          .catch(function(error) {
+            console.log(error);
+          });
     }
 
-    if(user.phoneNumber)
-    {
-      firebase.firestore().collection('users').doc(currentUser.uid).update({
-        phoneNumber: user.phoneNumber
-      });
+    if (user.phoneNumber) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(currentUser.uid)
+        .update({
+          phoneNumber: user.phoneNumber
+        })
+        .then(function() {
+        window.location.href = "/profile";
+      })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
 
-    if(user.photoFile)
-    {
-      var storageRef = firebase.storage().ref().child(currentUser.uid);
+    if (user.photoFile) {
+      var storageRef = firebase
+        .storage()
+        .ref()
+        .child(currentUser.uid);
       await storageRef.put(user.photoFile[0]).then(function(snapshot) {
-        console.log('Uploaded a blob or file!');
+        console.log("Uploaded a blob or file!");
+        
       });
 
       var uploadTask = storageRef.put(user.photoFile[0]);
@@ -164,43 +149,60 @@ export const updateUser = (user) => async (
       // 1. 'state_changed' observer, called any time the state changes
       // 2. Error observer, called on failure
       // 3. Completion observer, called on successful completion
-      uploadTask.on('state_changed', function(snapshot){
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) 
-        {
-          case firebase.storage.TaskState.PAUSED: // or 'paused'
-            console.log('Upload is paused');
-            break;
-          case firebase.storage.TaskState.RUNNING: // or 'running'
-            console.log('Upload is running');
-            break;
-          default:
-            console.log('continue....')
+      uploadTask.on(
+        "state_changed",
+        function(snapshot) {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          var progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log("Upload is paused");
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log("Upload is running");
+              break;
+            default:
+              console.log("continue....");
+          }
+        },
+        function(error) {
+          console.log("Your upload is not successful.")
+        },
+        function() {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+            console.log("File available at", downloadURL);
+            currentUser.updateProfile({
+              photoURL: downloadURL
+            });
+            firebase
+              .firestore()
+              .collection("users")
+              .doc(currentUser.uid)
+              .update({
+                photoURL: downloadURL
+              })
+              .then(function() {
+                window.location.href = "/profile";
+              })
+                .catch(function(error) {
+                  console.log(error);
+                });
+          });
         }
-      }, function(error) {
-        // Handle unsuccessful uploads
-      }, function() {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-          console.log('File available at', downloadURL);
-          currentUser.updateProfile({
-            photoURL: downloadURL
-        })
-        firebase.firestore().collection('users').doc(currentUser.uid).update({
-          photoURL: downloadURL
-        });
-        });
-      });
+      );
+      // window.location.href = "/profile";
     }
-    window.location.href = "/profile";
+
   } catch (error) {
     console.log(error);
     throw new SubmissionError({
       _error: error.message
     });
   }
+  // window.location.href = "/profile";
 };
--- src/features/user/UserProfile.jsx
+++ src/features/user/UserProfile.jsx
@@ -1,36 +1,111 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux'
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
+import { connect } from "react-redux";
+import { withFirestore, isLoaded, isEmpty } from 'react-redux-firebase'
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+const actions = {};
+
+const mapState = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile,
+});
 
 
-const actions = {
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+});
 
+function CircularIndeterminate() {
+  return (
+    <div>
+      <CircularProgress/>    
+    </div>
+  );
 }
 
 
-const mapState = (state) => ({
-    auth: state.firebase.auth,
-    profile: state.firebase.profile
-})
+var showPhone = null;
 
 
 class UserProfile extends Component {
-  render(){
-    const { auth } = this.props;
-    console.log(auth);
-    return (
-    <div>
-      <h1>User Profile</h1>
-      {/* <h2>Name: {window.name}</h2>
-      <h2>Email: {window.email}</h2>
-      <h2>photoURL: {window.photoURL}</h2> */}
-      <img width="200" height= "200" src={auth.photoURL} alt=""/>
-      <h2>Name: {auth.displayName}</h2>
-      <h2>Email: {auth.email}</h2>
-      <Button href="/profileEdit">Update Profile</Button>
-    </div>
-  );
-};
-};
 
-export default connect(mapState,actions)(UserProfile);
+
+  async componentDidMount() {
+    try
+    {
+      const { auth } = this.props;
+      const {firestore, firebase} = this.props;
+
+      var currentUser;
+      firebase
+      .auth()
+      .onAuthStateChanged(function(user){
+
+        
+          currentUser = user;
+          var userId = currentUser.uid;
+   
+          var docRef = firebase.firestore().collection("users").doc(userId);
+      
+          docRef.get().then(function(doc) {
+              if (doc.exists) {
+                  console.log("Document data:", doc.data().phoneNumber);
+                  auth.phoneNumber = doc.data().phoneNumber;
+              } else {
+                  // doc.data() will be undefined in this case
+                  console.log("No such document!");
+              }
+          }).catch(function(error) {
+              console.log("Error getting document:", error);
+          });
+      });
+
+
+    }
+    catch (error) {
+      console.log(error);
+    }
+    
+  }
+
+  render() {
+    const { auth } = this.props;
+    
+    if(isLoaded(auth) || !isEmpty(auth))
+    {
+      if(!auth.isEmpty)
+      {
+        return (
+          <div>
+            <h1>User Profile</h1>
+            <img width="200" height="200" src={auth.photoURL} alt="" />
+            <h2>Name: {auth.displayName}</h2>
+            <h2>Email: {auth.email}</h2>
+            <h2>Phone Number: {auth.phoneNumber} </h2>
+            <Button href="/profileEdit">Update Profile</Button>
+          </div>
+        );
+      }
+      else
+      {
+        return (window.location.replace("/login"))
+      }
+    }
+
+    else
+    {
+      return (
+        <CircularIndeterminate></CircularIndeterminate>
+      )
+    }
+
+  }
+}
+
+export default withStyles(styles)(withFirestore(connect(
+  mapState,
+  actions
+)(UserProfile)));
