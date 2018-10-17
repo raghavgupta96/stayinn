6f519a4336daf24f6a0a66bce5122024dae2a1a7
Gideon <gideon.ubaldo@sjsu.edu>
2018-10-16 17:02:38 -0700

    added back the login function

--- src/features/nav/navbar/NavBar.jsx
+++ src/features/nav/navbar/NavBar.jsx
@@ -1,30 +1,76 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
+import { withFirebase } from "react-redux-firebase";
+
+const mapState = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile
+});
 
 // style={{ flex: 1 }} pushes the subsequent things to the right
-const NavBar = theme => {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="headline" color="inherit" style={{ flex: 1 }}>
-          <IconButton href="/Home">StayInn</IconButton>
-        </Typography>
-        {/* not sure if as={NavLink} is the right way... */}
-        {/* <Button as={NavLink} to='/login' color='inherit'>Login</Button> | */}
-        <Button href="/login" color="inherit">
-          Login
-        </Button>
-        <Button href="/signup" color="inherit">
-          Signup
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
-};
+class NavBar extends Component {
+  handleSignOut = () => {
+    this.props.firebase.logout();
+    this.props.history.push("/");
+  };
+  render() {
+    // auth contains all user information (Ex: email, user, displayName, etc.)
+    const { auth } = this.props;
+    const authenticated = auth.isLoaded && !auth.isEmpty && auth.emailVerified;
+    //console.log(auth);
+    //console.log(authenticated);
 
-export default withRouter(NavBar);
+    // Check if auth is loaded before displaying navbar
+    // should solve the issue where there is flash of "login"
+    // even though a user logs in
+    return (
+      auth.isLoaded && (
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="headline" color="inherit" style={{ flex: 1 }}>
+              <IconButton href="/">StayInn</IconButton>
+            </Typography>
+            {authenticated ? (
+              <div>
+                {/* Use Link to direct user to the profile URL with user ID */}
+                <Link
+                  style={{ color: "black", textDecoration: "none" }}
+                  to={`/profile/${auth.uid}`}
+                >
+                  <Button color="inherit">{auth.displayName}</Button>
+                </Link>
+                <Button href="/" color="inherit" onClick={this.handleSignOut}>
+                  Sign Out
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <Button href="/login" color="inherit">
+                  Login
+                </Button>
+                <Button href="/signup" color="inherit">
+                  Signup
+                </Button>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      )
+    );
+  }
+}
+
+export default withRouter(
+  withFirebase(
+    connect(
+      mapState,
+      null
+    )(NavBar)
+  )
+);
