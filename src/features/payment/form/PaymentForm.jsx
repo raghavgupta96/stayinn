10057854b5438e5cd0ee7ff0f9bc574889dcf046
Patrick Garcia <pkmgarcia@gmail.com>
2018-11-04 06:52:00 -0800

    Removed unnecessary import

--- src/features/payment/form/PaymentForm.jsx
+++ src/features/payment/form/PaymentForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
